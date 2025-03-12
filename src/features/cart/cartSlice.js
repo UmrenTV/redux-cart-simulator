7a12a961820b5852dcd4e2a67b5a1d9848afe607
Jane Zdravevski <jane@zdravevski.pro>
2025-03-13 00:19:23 +0100

    swapped the fixed cartItems with the ones that are fetched from the API + cleaned some console.logs and stuffs not used

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,10 +1,8 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import cartItems from "../../cartItems";
-import { openModal } from "../modal/modalSlice"; // example use  below in thunkAPI
 
 const initialState = {
-    cartItems: cartItems,
+    cartItems: [],
     amount: 0, // total amount of items in the cart
     total: 0, // total price of all items in the cart
     isLoading: true,
@@ -17,23 +15,13 @@ export const getCartItems = createAsyncThunk(
     async (random, thunkAPI) => {
         try {
             console.log(random);
-            //console.log(thunkAPI.getState()); // this is complete state, of all features we've set up, which is very useful
-            //thunkAPI.dispatch(openModal()); // you can do lots of things with thunkAPI, like open modal, or dispatch other actions from other features
             const response = await axios.get(url);
             return response.data;
         } catch (error) {
             console.log(error);
             //return thunkAPI.rejectWithValue(error.response.data);
             return thunkAPI.rejectWithValue("error fetching");
-            // if you mess up with the url, you will get an error, and you can catch it here, and then you can return the error message to the user
-            // // I've added console.log in the 'rejected' case below, so you can see the error message in the console
         }
-
-        // return fetch(url)
-        //     .then((response) => response.json())
-        //     .catch((error) => {
-        //         console.log(error);
-        //     });
     }
 );
 
@@ -46,7 +34,6 @@ const cartSlice = createSlice({
                 state.isLoading = true;
             })
             .addCase(getCartItems.fulfilled, (state, action) => {
-                // console.log(action.payload);
                 state.isLoading = false;
                 state.cartItems = action.payload;
             })
@@ -57,12 +44,9 @@ const cartSlice = createSlice({
     },
     reducers: {
         clearCart: (state) => {
-            //state.cartItems = [];
             return {
                 ...state,
                 cartItems: [],
-                // if we don't spread the ...state, and just modify cartItems when returning the state,
-                // then all of the other state properties will be removed, and only cartItems will exist in the state.
             };
         },
         removeItem: (state, action) => {
@@ -80,7 +64,6 @@ const cartSlice = createSlice({
                 return item.id === payload;
             });
             cartItem.amount += 1;
-            // you can also modify the state directly like this, compared to above where I return the state
         },
         decrease: (state, { payload }) => {
             const cartItem = state.cartItems.find((item) => {
@@ -107,7 +90,5 @@ const cartSlice = createSlice({
 
 export const { clearCart, removeItem, increase, decrease, calculateTotals } =
     cartSlice.actions;
-
-// console.log(cartSlice);
 
 export default cartSlice.reducer;
